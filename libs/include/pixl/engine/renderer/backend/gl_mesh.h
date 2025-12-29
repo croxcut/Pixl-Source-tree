bfb3a567b621e7edfbd394e7dc0265918dfef6ee
AppaulPie <133219813+croxcut@users.noreply.github.com>
2025-12-30 05:02:10 +0800

    just minor fixes

--- libs/include/pixl/engine/renderer/backend/gl_mesh.h
+++ libs/include/pixl/engine/renderer/backend/gl_mesh.h
@@ -9,8 +9,11 @@ private:
     u32 vao, vbo;
     size_t size;
 
-public:
+    u32 get_vao() const { return vao; }
+    u32 get_vbo() const { return vbo; }
+    u32 get_size() const { return size; }
 
+public:
     void create_mesh(struct Mesh& mesh);
     void draw() const;
 };
