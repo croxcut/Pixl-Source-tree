8586a6f60d34ea6189d1d9c4a97fc60a910e61bb
AppaulPie <133219813+croxcut@users.noreply.github.com>
2026-01-13 23:38:02 +0800

    added a simple mesh loader simple (so it should be improved later down the line)

---
+++ backend/rendering/gl41_mesh_loader.cpp
@@ -0,0 +1,37 @@
+#include "gl41_mesh_loader.h"
+
+#include "misc/utility/log.h"
+
+#include <glad/glad.h>
+
+GL41MeshLoader::GL41MeshLoader(struct Mesh& mesh) {
+
+    if(mesh.verticies.empty()) {
+        WARN("Vertices are empty: %lld", 
+            mesh.verticies.size());
+        return;
+    }
+
+    glGenVertexArrays(1,&mesh.vao);
+    glBindVertexArray(mesh.vao);
+
+    glGenBuffers(1, &mesh.vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, mesh.vbo);
+    glBufferData(GL_ARRAY_BUFFER, mesh.verticies.size() * sizeof(Vertex), mesh.verticies.data(), GL_STATIC_DRAW);
+
+    glGenBuffers(1, &mesh.ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh.ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, mesh.indices.size() * sizeof(u32), mesh.indices.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*) (3 * sizeof(f32)));
+    glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*) (6 * sizeof(f32)));
+    glEnableVertexAttribArray(2);
+
+    mesh.size = mesh.indices.size();
+}
+
--- backend/rendering/gl41_mesh_loader.h
+++ backend/rendering/gl41_mesh_loader.h
@@ -1,7 +1,13 @@
 #ifndef GL41_MESH_LOADER_H
 #define GL41_MESH_LOADER_H
 
+#include "backend/renderer_backend.h"
+
 class GL41MeshLoader {
+
+public:
+    GL41MeshLoader(struct Mesh& mesh);
+
     
 };
 
