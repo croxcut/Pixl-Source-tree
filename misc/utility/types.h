91cfacf2052b5ac07bb812bcee843e6d5038a68d
AppaulPie <133219813+croxcut@users.noreply.github.com>
2026-01-26 08:13:23 +0800

    refactored types

--- misc/utility/types.h
+++ misc/utility/types.h
@@ -32,17 +32,17 @@
 #include <stdint.h>
 #include <inttypes.h>
 
-typedef uint8_t     u8;
-typedef uint16_t    u16;
-typedef uint32_t    u32;
-typedef uint64_t    u64;
+typedef uint8_t     u8_t;
+typedef uint16_t    u16_t;
+typedef uint32_t    u32_t;
+typedef uint64_t    u64_t;
 
-typedef int8_t      s8;
-typedef int16_t     s16;
-typedef int32_t     s32;
-typedef int64_t     s64;
+typedef int8_t      s8_t;
+typedef int16_t     s16_t;
+typedef int32_t     s32_t;
+typedef int64_t     s64_t;
    
-typedef float       f32;
-typedef double      f64;
+typedef float       f32_t;
+typedef double      f64_t;
 
 #endif
